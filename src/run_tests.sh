2c38e839a3d3e4a28a91ce428ddf2fe4421f9445
Matejh36 <matejh36@gmail.com>
2019-12-05 23:27:46 +0100

    small fix in script

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -25,35 +25,36 @@ return_code="$?";
 PYTHON_OUTPUT="$(python3 < "${file}" 2> /dev/null)";
 # návratový kód pythonu
 
-if [ $return_code == $COMPILER_RETURN_CODE ]; then
-  	INTERPRED_OUTPUT="$(./ic19int output)";
-  	interpret_ret_code="$?";
-
+if [ $return_code = $COMPILER_RETURN_CODE ]; then
 	if [ $return_code != 0 ]; then
-    printf "===> ${GREEN}SUCCESS\n${NC}";
-    let "succes += 1";
+    	printf "===> ${GREEN}SUCCESS\n${NC}";
+    	let "succes += 1";
 	else
+		INTERPRED_OUTPUT="$(./ic19int output)";
+  		interpret_ret_code="$?";
 
 		INTERPRETER_RETURN_CODE="$(awk -F: -v src=$testing_file '{ if ($1 == src) print $3}' outputs/outputs)";
 
-		if [ "$interpret_ret_code" == "$INTERPRETER_RETURN_CODE" ]; then
-			if [ "$INTERPRED_OUTPUT" == "$PYTHON_OUTPUT" ]; then
+		if [ "$interpret_ret_code" = "$INTERPRETER_RETURN_CODE" ]; then
+			i_out="$(echo $INTERPRED_OUTPUT | tr -d '\n')";
+			p_out="$(echo $PYTHON_OUTPUT | tr -d '\n')";
+
+			if [ "$i_out" = "$p_out" ]; then
 				printf "===> ${GREEN}SUCCESS\n${NC}";
-  			let "succes += 1";
-		  else
+  				let "succes += 1";
+			else
 				printf "===> ${RED}FAILED\n${NC}";
-  			printf "Expected output: $PYTHON_OUTPUT get: $INTERPRED_OUTPUT\n";
-  			let "failed += 1";
+  				printf "Expected output: $PYTHON_OUTPUT get: $INTERPRED_OUTPUT\n";
+  				let "failed += 1";
 			fi
 		else
-			echo "nie"
-			#printf "Expected: $INTERPRETER_RETURN_CODE Interpred returned: $interpret_return_code";
+			printf "Expected: $INTERPRETER_RETURN_CODE Interpred returned: $interpret_return_code";
 		fi
 	fi
 else
 	printf "===> ${RED}FAILED\n${NC}";
 	printf "Expected return code: $COMPILER_RETURN_CODE get: $return_code\n";
-  let "failed += 1";
+	let "failed += 1";
 fi
 
 let "testNumber += 1";
